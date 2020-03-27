4cb53d896822987406d7f67ade473e1ac8809a4a
LittleJake <465917717@qq.com>
2020-03-27 16:44:44 +0800

    update Readme

--- TOTP/settings.py
+++ TOTP/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'TOTP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'totp',  # 数据库名，先前创建的
-        'USER': 'root',  # 用户名，可以自己创建用户
-        'PASSWORD': '',  # 密码
-        'HOST': '127.0.0.1',  # mysql服务所在的主机ip
-        'PORT': '3306',  # mysql服务端口
+        'NAME': 'totp',  # database
+        'USER': 'root',  # username
+        'PASSWORD': '',  # password
+        'HOST': '127.0.0.1',  # mysql server ip
+        'PORT': '3306',  # mysql server port
     }
 }
 
